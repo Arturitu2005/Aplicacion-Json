7d2ed8490d1180fced863b10129fcec907c34882
Arturitu2005 <garcia.lopez.arturo2005@gmail.com>
2022-10-16 21:03:02 -0500

    Aplicacion Terminada

---
+++ app/src/main/java/com/example/appfull/Olvido.java
@@ -0,0 +1,25 @@
+package com.example.appfull;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Olvido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvido);
+        Button Volver = findViewById(R.id.volver);
+        Volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Olvido.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
