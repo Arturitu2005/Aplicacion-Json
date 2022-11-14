95cfcf995e3e6fa0b358e82833287be51c0844fa
Arturitu2005 <garcia.lopez.arturo2005@gmail.com>
2022-11-13 19:53:12 -0600

    Proyecto con lista

--- app/src/main/java/com/example/appfull/welcome_to_the_jungle.java
+++ app/src/main/java/com/example/appfull/welcome_to_the_jungle.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.appfull.Json.MyInfo;
+import com.example.appfull.Json.MyData;
+import com.example.appfull.MyAdapter.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class welcome_to_the_jungle extends AppCompatActivity {
+    private ListView listView;
+    private List<MyData> list;
     private TextView user;
+    private int []imagen = { R.drawable.coladegato,R.drawable.zombot,R.drawable.lanzaguisantes,R.drawable.lanzamaiz};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +29,46 @@ public class welcome_to_the_jungle extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_to_the_jungle);
         String aux = null;
         MyInfo info = null;
+        MyData myData = null;
         Object object = null;
         user = findViewById(R.id.presId);
+        listView = (ListView) findViewById(R.id.listViewId);
+        list = new ArrayList<MyData>();
         Intent intent = getIntent();
+
+        for (int i = 0; i < 4; i++){
+            myData = new MyData();
+            myData.setContra(String.format("Contraseña: %d",(int)(Math.random()*10000)));
+
+            if (i == 0){
+                myData.setRed(String.format( "Rabo de gato"));
+                myData.setImage(imagen[0]);
+            }
+            if (i == 1){
+                myData.setRed(String.format( "Zombot"));
+                myData.setImage(imagen[1]);
+            }
+            if (i == 2){
+                myData.setRed(String.format( "Lanzaguisante" ));
+                myData.setImage(imagen[2]);
+            }
+            if (i == 3){
+                myData.setRed(String.format( "Lanzamaiz" ));
+                myData.setImage(imagen[3]);
+            }
+            list.add(myData);
+        }
+        MyAdapter myAdapter = new MyAdapter(list, getBaseContext());
+        listView.setAdapter(myAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                toast( i );
+            }
+        });
+
+
         if (intent != null){
             aux = intent.getStringExtra("Usuario");
             if (aux != null && aux.length()>0){
@@ -34,7 +84,8 @@ public class welcome_to_the_jungle extends AppCompatActivity {
                 }
             }
         }
-
-
+        }
+        private void toast(int i){
+            Toast.makeText(getBaseContext(), list.get(i).getContra(),Toast.LENGTH_SHORT).show();
+        }
     }
-}
